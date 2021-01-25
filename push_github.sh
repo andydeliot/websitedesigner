52f4f6b6b05be5d6ff588856fe132860c9253563
Andy <A.andy@hotmail.fr>
2021-01-25 11:11:59 +0100

    Modif push_github

--- push_github.sh
+++ push_github.sh
@@ -9,6 +9,15 @@ read repertoire
 
 echo ""
 
+git status
+echo "Ctrl+c pour quitter si l'ajout de ses fichiers ne conviennent pas."
+read nothing
+git add --all
+git status
+echo "Message de commit : "
+read message
+git commit -m "$message"
+
 git remote add origin https://github.com/"$username"/"$repertoire".git
 git branch -M main
 git push -u origin main
